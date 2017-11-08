1a942d3c9103ff911ab3ec5828c36ae5208bb716
Yoscroll <ianhenry.c808@gmail.com>
2017-11-08 10:33:06 -1000

    cleaned code

--- client-side/app.js
+++ client-side/app.js
@@ -1,17 +1,11 @@
 console.log("hi");
-
-$('#getPics').click(() => {
- 	getImg();
- });
-
 let getImg = () => $.ajax({
 	url: 'https://q4io62yr1e.execute-api.us-west-2.amazonaws.com/dev/users/photos',
 	type: 'GET',
-	'Content-Type': 'application/json',
-	success: (data) => (buildHtmlElements(data.message.Contents), console.log(data)),
+	success: (data) => buildHtmlElements(data.message.Contents),
 });
 let buildHtmlElements = (responseData) =>(
 	responseData.map((pic)=>{
 		$('#pictures').append(`<div class="image"><img src="https://s3-us-west-2.amazonaws.com/photo-bucket-tmp-prjct/${pic.Key}"></div>`)
-      })
-	);
+    })
+);
--- handler.js
+++ handler.js
@@ -1,23 +1,7 @@
 'use strict';
-
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const params = { Bucket: 'photo-bucket-tmp-prjct'};
-
 module.exports.photos= (event, context, callback) => {
   s3.listObjects(params, function(err, data) {
     if (err){
